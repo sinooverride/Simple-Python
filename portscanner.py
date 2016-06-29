9c9b1be30ac226215fb6d5b8212a8d0be4079316
Sino <sino@fritzbox.com>
2016-06-29 09:23:39 +0200

    update

--- portscanner.py
+++ portscanner.py
@@ -2,33 +2,19 @@ import socket
 import subprocess
 import sys
 
-def scan_host():
+def scan_host(startport, endport, serverIP):
     i = 0
-    while True:
-        try:
-            server = input("Enter host: ")
-            serverIP = socket.gethostbyname(server)
-            print("Define portrange")
-            startport = int(input("From: "))
-            endport = int(input("To: "))
-            print("\n")
-            print("scanning for {0} with IP: {1} on Port {2}-{3}".format(server,serverIP,startport,endport))
-            print("...")
+    for port in range(startport,endport):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        output = s.connect_ex((serverIP, port))
+        if output == 0:
+            print ("Port {}:  Open".format(port))
+            i = +1
+            s.close()
+    print("\n")
+    if i == 0:
+        print("Open Ports:  0!")
 
-            for port in range(startport,endport):
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                output = s.connect_ex((serverIP, port))
-                if output == 0:
-                    print ("Port {}:  Open".format(port))
-                    i = +1
-                s.close()
-            print("\n")
-            if i == 0:
-                print("Open Ports:  0!")
-            return False
-
-        except socket.gaierror:
-            print("no servername...")
 
 
 if __name__ == "__main__":
@@ -38,7 +24,21 @@ if __name__ == "__main__":
     print("-"*20)
     print("\n")
 
-    scan_host()
+    try:
+        server = input("Enter host: ")
+        serverIP = socket.gethostbyname(server)
+        print("Define portrange")
+        startport = int(input("From: "))
+        endport = int(input("To: "))
+        print("\n")
+        print("scanning for {0} with IP: {1} on Port {2}-{3}".format(server, serverIP, startport, endport))
+        print("...")
+        scan_host(startport,endport,serverIP)
+    except socket.gaierror:
+        print("no servername...")
+        pass
+
+
 
 
 
